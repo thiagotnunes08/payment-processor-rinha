ba56a66e067af6e6379f3282a0de5de31ddee6c3
thiago.nunes <thiago.nunes@perbank.com.br>
2025-07-22 15:32:21 -0300

    add config reflections

--- src/main/java/payement/worker/rinha/dto/PaymentRequestProcessor.java
+++ src/main/java/payement/worker/rinha/dto/PaymentRequestProcessor.java
@@ -1,8 +1,10 @@
 package payement.worker.rinha.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
-
+@RegisterForReflection
 public record PaymentRequestProcessor(UUID correlationId, BigDecimal amount, Instant requestedAt) {
 }
